6d029aadfe29d8540a6a9295b6e8400c9b58951f
Alice Lin <alicexxlin@gmail.com>
2023-08-25 12:40:52 -0600

    complete exercise 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+  if (startNum < 0 || endNum < 0) return "ERROR";
+  if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR";
 
+  if (startNum > endNum) {
+    [startNum, endNum] = [endNum, startNum];
+  }
+
+  let sum = 0;
+  for (let i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

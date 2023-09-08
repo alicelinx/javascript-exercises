47846d2c2b639292eee09b1ddc95154ce8a730e7
Alice Lin <alicexxlin@gmail.com>
2023-09-08 14:22:37 -0600

    Complete function 'power'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(array) {
   return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
 const factorial = function() {

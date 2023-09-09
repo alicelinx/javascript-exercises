f7a7d0995befb2561a1b6b27ee1d3717a482fc3e
Alice Lin <alicexxlin@gmail.com>
2023-09-09 12:48:00 -0600

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  const processedStr = str.toLowerCase().replace(/[^A-Z0-9]+/ig, '');
+  return processedStr === processedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line

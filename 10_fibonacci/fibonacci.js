e519fc18a25b3daee3af2667196287f7b477dd4d
Alice Lin <alicexxlin@gmail.com>
2023-09-09 13:05:57 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
 
+  let firstNum = 1;
+  let secondNum = 0;
+
+  for (let i = 2; i <= count; i++) {
+    let currentNum = firstNum + secondNum;
+    secondNum = firstNum;
+    firstNum = currentNum;
+  }
+  return firstNum;
 };
 
 // Do not edit below this line

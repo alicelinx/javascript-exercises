e85ad0d45009a61a75be7c3c593e273503c38120
Alice Lin <alicexxlin@gmail.com>
2023-09-11 14:35:31 -0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((oldestPerson, currentPerson) => {
+    let oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge > currentAge ? oldestPerson : currentPerson;
+  });
+};
 
+const getAge = function(birthYear, deathYear) {
+  if (!deathYear) {
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear;
 };
 
 // Do not edit below this line
